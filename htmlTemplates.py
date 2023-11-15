890de353540b00eb24980702ab258973de54d633
StuckNSpace <34696551+StuckNSpace@users.noreply.github.com>
2023-11-14 22:11:47 -0600

    Update htmlTemplates.py
    Added Message Fade In

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,37 +1,64 @@
 css = '''
 <style>
-.chat-message {
-    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
-}
-.chat-message.user {
-    background-color: #2b313e
-}
-.chat-message.bot {
-    background-color: #475063
-}
-.chat-message .avatar {
-  width: 20%;
-}
-.chat-message .avatar img {
-  max-width: 520px;
-  max-height: 520px;
-  border-radius: 50%;
-  object-fit: cover;
-}
-.chat-message .message {
-  width: 80%;
-  padding: 0 1.5rem;
-  color: #fff;
-}
-'''
+    body {
+        font-family: 'Lato', sans-serif; /* Use a custom font */
+        background-color: #f0f0f0; /* Set a background color for the whole page */
+        margin: 0;
+        padding: 0;
+    }
 
-bot_template = '''
-<div class="chat-message bot">
-    <div class="avatar">
-        <img src="src/HelpfulKitten.png">
-    </div>
-    <div class="message">{{MSG}}</div>
-</div>
+    .chat-container {
+        background-color: #fff; /* Set a background color for the chat container */
+        border-radius: 10px; /* Add rounded corners */
+        overflow: hidden; /* Hide overflow content */
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Add a subtle shadow effect */
+    }
+
+    .chat-message {
+        padding: 1.5rem;
+        border-radius: 0.5rem;
+        margin-bottom: 1rem;
+        display: flex;
+        animation: fadeIn 0.5s ease; /* Add fadeIn animation */
+    }
+
+    .chat-message.user {
+        background-color: #2b313e;
+        color: #fff; /* Set text color for user messages */
+    }
+
+    .chat-message.bot {
+        background-color: #475063;
+        color: #fff; /* Set text color for bot messages */
+    }
+
+    .chat-message .avatar {
+        width: 20%;
+    }
+
+    .chat-message .avatar img {
+        max-width: 78px;
+        max-height: 78px;
+        border-radius: 50%;
+        object-fit: cover;
+    }
+
+    .chat-message .message {
+        width: 80%;
+        padding: 0 1.5rem;
+    }
+
+    /* Keyframe animation for fadeIn effect */
+    @keyframes fadeIn {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+</style>
+
 '''
 
 user_template = '''
