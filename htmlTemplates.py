bbb102f7954f00c0f39b06597a3a166de02ea994
StuckNSpace <34696551+StuckNSpace@users.noreply.github.com>
2023-11-21 17:21:10 -0600

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -61,6 +61,15 @@ css = '''
 
 '''
 
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.pn>
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
